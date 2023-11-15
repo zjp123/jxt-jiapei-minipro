796cc6305a640db56033d954dd2265db3235d2d2
zhangjunpeng <zhangjunpeng@58.com>
2023-11-15 10:41:37 +0800

    feat: 底部tab初始化

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,30 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/consultation/consultation',
+    'pages/registration/registration',
+    'pages/my/my',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: '首页',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    "list": [{
+      "pagePath": "pages/index/index",
+      "text": "首页"
+    },{
+      "pagePath": "pages/consultation/consultation",
+      "text": "咨询"
+    },{
+        "pagePath": "pages/registration/registration",
+        "text": "报名"
+      },{
+        "pagePath": "pages/my/my",
+        "text": "我的"
+      }
+    ]
+  },
 })
---
+++ src/pages/registration/registration.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import './registration.less'
+
+export default function Index() {
+
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  return (
+    <View className=''>
+      <Text>报名</Text>
+    </View>
+  )
+}
