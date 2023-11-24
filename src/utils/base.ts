cd7cc5555d3fb657d6db279216f17c385905189d
董晓磊 <dongxiaolei@58.com>
2023-11-24 17:30:03 +0800

    feat: 1124-报名调试-dongxiaolei

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -11,10 +11,10 @@ const notOnlineApi = process.env.NODE_ENV === 'development' || process.env.NODE_
 // OPEN_API: '"http://jxtopenapi.58v5.cn"',  https://jxtopenapi.xueche.com
 
 const URLLIST: URL = {
-    BASE_URL: notOnlineApi ? 'http://jxtopenapi.58v5.cn' : 'https://jxtopenapi.xueche.com',
-    BASE_SAAS: notOnlineApi ? 'https://jxtsaas.58v5.cn' : 'https://saas.xueche.com',
-    GATE_URL: notOnlineApi ? 'https://gatewayapi.58v5.cn/jxtsaas_0' : 'https://saas.xueche.com',
-    JXTCOMMON_API: notOnlineApi ? 'https://jxt-common.58v5.cn' : 'https://jxt-common.xueche.com',
+    BASE_URL: 'http://jxtopenapi.58v5.cn',
+    BASE_SAAS: 'https://jxtsaas.58v5.cn',
+    GATE_URL: 'https://gatewayapi.58v5.cn/jxtsaas_0',
+    JXTCOMMON_API: 'https://jxt-common.58v5.cn',
     // GATE_URL: 'https://saas.xueche.com'
 }
 console.log("看看环境是测试环境吗",notOnlineApi,URLLIST.BASE_URL,URLLIST)
