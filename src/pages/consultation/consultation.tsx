3eb7785b2d0b8a67b50acfdf04a1c534d90c9b22
jiaolong <jiaolong@58.com>
2023-11-24 11:46:03 +0800

    feat: 接入智齿

--- src/pages/consultation/consultation.tsx
+++ src/pages/consultation/consultation.tsx
@@ -1,13 +1,21 @@
-import { View, WebView } from '@tarojs/components';
+import { View, WebView } from '@tarojs/components'
+import Taro, { useLoad } from '@tarojs/taro'
 
 const Consultation = () => {
+
+    useLoad(() =>{
+        setTimeout(() => {
+            Taro.setNavigationBarTitle({ title: '咨询' }) // 设置自定义标题
+        },2000)
+    })
+
     return (
         <View>
             <WebView
                 src={'https://jxt.soboten.com/chat/h5/v6/index.html?sysnum=4d43f6416ac84028adb1d29bede67376&source=10'}
             />
         </View>
-    );
-};
+    )
+}
 
-export default Consultation;
+export default Consultation
