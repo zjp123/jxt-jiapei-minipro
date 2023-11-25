fed4c3c53596c2915fb4eae23c53d9a1f40565c0
董晓磊 <dongxiaolei@58.com>
2023-11-25 13:49:44 +0800

    feat: 1124-报名调试-dongxiaolei

--- src/pages/news/news.tsx
+++ src/pages/news/news.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View } from '@tarojs/components'
+import { View, ScrollView } from '@tarojs/components'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 import Newlist from './newlist'
 import './news.scss'
@@ -13,25 +13,32 @@ const Index: React.FC = () => {
 
     return (
         <View className='news-werrper'>
-            <AtTabs
-                animated={false}
-                current={current}
-                tabList={[
-                    { title: '驾校新闻' },
-                    { title: '驾校公告' },
-                    { title: '常见问题' }
-                ]}
-                onClick={changeCurrent}>
-                <AtTabsPane current={current} index={0} >
-                    <Newlist current={current} />
-                </AtTabsPane>
-                <AtTabsPane current={current} index={1}>
-                   <Newlist current={current} />
-                </AtTabsPane>
-                <AtTabsPane current={current} index={2}>
-                    <Newlist current={current} />
-                </AtTabsPane>
-            </AtTabs>
+            <ScrollView
+                scrollY
+                style={{ height: '100%', backgroundColor: '#F8F8F8', overflow: 'hidden' }}
+            >
+                <View style={{height: '1px'}}></View>
+                <AtTabs
+                    animated={false}
+                    current={current}
+                    tabList={[
+                        { title: '驾校新闻' },
+                        { title: '驾校公告' },
+                        { title: '常见问题' }
+                    ]}
+                    onClick={changeCurrent}>
+                    <AtTabsPane current={current} index={0} >
+                        <Newlist current={current} />
+                    </AtTabsPane>
+                    <AtTabsPane current={current} index={1}>
+                        <Newlist current={current} />
+                    </AtTabsPane>
+                    <AtTabsPane current={current} index={2}>
+                        <Newlist current={current} />
+                    </AtTabsPane>
+                </AtTabs>
+            </ScrollView>
+
         </View>
     )
 }
