6fa747a5ed95f0f76ed0bf43a7e35078d80b3ab2
zhangjunpeng <zhangjunpeng@58.com>
2023-11-28 11:43:49 +0800

    feat: 接口环境判断

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -5,19 +5,20 @@ interface URL {
     JXTCOMMON_API: string
 }
 
-const notOnlineApi = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+// const isDev = process.env.NODE_ENV === 'development'
+const isDev = false
 
 
 // OPEN_API: '"http://jxtopenapi.58v5.cn"',  https://jxtopenapi.xueche.com
 
 const URLLIST: URL = {
-    BASE_URL: 'http://jxtopenapi.58v5.cn',
-    BASE_SAAS: 'https://jxtsaas.58v5.cn',
-    GATE_URL: 'https://gatewayapi.58v5.cn/jxtsaas_0',
+    BASE_URL: isDev ? 'http://jxtopenapi.58v5.cn' : 'https://jxtopenapi.xueche.com',
+    BASE_SAAS: isDev ? 'https://jxtsaas.58v5.cn' : 'https://saas.xueche.com',
+    GATE_URL: isDev ? 'https://gatewayapi.58v5.cn/jxtsaas_0' : 'https://saas.xueche.com',
     JXTCOMMON_API: 'https://jxt-common.58v5.cn',
     // GATE_URL: 'https://saas.xueche.com'
 }
-console.log("看看环境是测试环境吗",notOnlineApi,URLLIST.BASE_URL,URLLIST)
+console.log("看看环境是测试环境吗", isDev, URLLIST.BASE_URL,URLLIST)
 //https://saas.xueche.com
 // http://jxedtsaastest3.58v5.cn
 // http://jxtsaas.58v5.cn
